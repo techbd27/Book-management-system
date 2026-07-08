88dd1b09847e1293533c994e3b3a80682e5ccb47
Pabitra Kumar Mandal <148943734+techbd27@users.noreply.github.com>
2026-07-08 12:42:02 +0600

    Update Django settings for production configuration

--- book_management_system/settings.py
+++ book_management_system/settings.py
@@ -24,9 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--!piudg8#_0bo=+a+_+mnfbisf@fm+m8-$$46^ez(qvmo4+i*d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 
+    ".vercel.app",
+    "127.0.0.1",
+    "localhost",
+]
 
 
 # Application definition
@@ -119,8 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
