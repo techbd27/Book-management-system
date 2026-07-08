f533763b4f47ca7160ec8aebd8bce3ea805985fd
Pabitra Kumar Mandal <148943734+techbd27@users.noreply.github.com>
2026-07-08 13:11:06 +0600

    Change DEBUG setting to False
    Set DEBUG to False for production environment.

--- book_management_system/settings.py
+++ book_management_system/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--!piudg8#_0bo=+a+_+mnfbisf@fm+m8-$$46^ez(qvmo4+i*d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = False
 
 ALLOWED_HOSTS = [ 
     ".vercel.app",
